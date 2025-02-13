714f00d9bfc6c9a26cff373345366f8b59f1d329
takuro <talangxiaotian6@gmail.com>
2025-02-13 23:11:54 +0900

    feat: Taskモデルファイルの設定の追加

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,3 +1,18 @@
 class Task < ApplicationRecord
   belongs_to :project
+
+  # バリデーション
+  validates :title, presence: true
+  validates :due_date, presence: true
+
+  # 独自バリデーション: 期限が今日以前にならないようにする
+  validate :due_date_cannot_be_in_the_past
+
+  private
+
+  def due_date_cannot_be_in_the_past
+    if due_date.present? && due_date < Date.today
+      errors.add(:due_date, "は本日以降の日付を選択してください")
+    end
+  end
 end
